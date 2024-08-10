58d76c96fecab721cf54336ea82919c68744af1a
Abdul Rahman <ar69king@gmail.com>
2024-08-11 02:29:55 +0500

    vercel

--- HiAnime_to_MAL/wsgi.py
+++ HiAnime_to_MAL/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HiAnime_to_MAL.settings")
 
 application = get_wsgi_application()
+
+app = application
