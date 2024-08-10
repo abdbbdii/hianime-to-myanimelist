bd2f8081f707e359299acf24518206691086656a
Abdul Rahman <ar69king@gmail.com>
2024-08-11 03:05:14 +0500

    static

--- HiAnime_to_MAL/settings.py
+++ HiAnime_to_MAL/settings.py
@@ -132,6 +132,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "converter" / "static",
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
